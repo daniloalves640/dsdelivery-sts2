8b2c4ce153ad9b842227ab66a1bbed5843f6b735
Danilo Alves <danilo_alves640@hotmail.com>
2021-01-08 19:51:38 -0300

    Search order location

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,12 +1,33 @@
 import './styles.css';
 import StepsHeader from "./StepsHeader";
+import ProductsList from "./ProductsList"
+import { useEffect, useState } from 'react';
+import {OrderLocationdata, Product} from "./types";
+import {fetchProducts} from "../api"
+import OrderLocation from "./OrderLocation";
 
 function Orders() {
+
+    const [products, setProducts] = useState<Product[]>([]);
+
+    const [orderLocation, setOrderLocation] = useState <OrderLocationdata> ()
+
+    //puxando os dados do backend e mostrando na api
+    //axios
+
+    useEffect(() => {
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error));
+
+    }, [])
     return (
         <div className="orders-container">
             <StepsHeader/>
+            <ProductsList products = {products}/>
+            <OrderLocation onChangeLocation={location => setOrderLocation (location)} />
         </div>
-    )
+   )
 }
 
 export default Orders;
---
+++ front-web/src/Orders/types.tsx
@@ -0,0 +1,15 @@
+// listando os tipos de dados usados na lista
+export type Product = {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    imageUri: string;
+}
+
+export type OrderLocationdata = {
+    latitude: number;
+    longitude: number;
+    address: string;
+
+}
